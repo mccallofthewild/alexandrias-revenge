59af01f26207b6aee3e05050ef3cf7b9957ff91d
mccallofthewild <mccallalexander96@gmail.com>
2020-01-16 17:30:41 -0700

    updated list loader

--- public/app.js
+++ public/app.js
@@ -17,11 +17,23 @@
 
 	// App
 
-	const state = {
-		parseResult: null,
-		previewHTML: null, // idk
-		publishedArchiveId: null
-	};
+	const state = (() => {
+		let pendingArticles = JSON.parse(
+			window.sessionStorage.getItem('pendingArticles') || '[]'
+		);
+		return {
+			parseResult: null,
+			previewHTML: null, // idk
+			publishedArchiveId: null,
+			get pendingArticles() {
+				return pendingArticles;
+			},
+			set pendingArticles(v) {
+				pendingArticles = v;
+				window.sessionStorage.setItem('pendingArticles', JSON.stringify(v));
+			}
+		};
+	})();
 	const refs = {
 		urlInput: document.getElementById('url-input'),
 		previewArea: document.getElementById('preview-area'),
@@ -45,7 +57,8 @@
 			}
 		},
 		setUrlInputValue(str) {
-			refs.urlInput.setAttribute('value', str);
+			console.log('setting input ', str, refs.urlInput);
+			refs.urlInput.value = str;
 		}
 	};
 
@@ -68,7 +81,7 @@
 			`();
 			return archivedArticles;
 		},
-		async loadArchivePublishStatus() {
+		async loadArchivePublishStatus(txId) {
 			const {
 				data: { archivePublishStatus }
 			} = await graphql`
@@ -76,30 +89,69 @@
 					archivePublishStatus(txId: $txId)
 				}
 			`({
-				txId: state.publishedArchiveId
+				txId: txId
 			});
 			return archivePublishStatus;
+		},
+		async pollForSuccessfulTransaction(txId) {
+			let count = 0;
+			while (true) {
+				if (count++ > 60 * 1000 * 12) {
+					alert('Archive process timed out');
+				}
+				try {
+					await new Promise(r => setTimeout(r, 1000));
+					const archivePublishStatus = await loaders.loadArchivePublishStatus(
+						txId
+					);
+					console.log(archivePublishStatus);
+					if (archivePublishStatus == 'SUCCESS') {
+						alert('Successfully archived ' + 'arweave.net/' + txId);
+						break;
+					}
+				} catch (e) {
+					console.error(e);
+				}
+			}
+			state.pendingArticles = state.pendingArticles.filter(a => a.id != txId);
+			connectors.updateArchivedArticlesList();
 		}
 	};
 
 	const connectors = {
 		async updateArchivedArticlesList() {
 			refs.archivedArticlesList.innerHTML = '';
-			const articles = await loaders.loadArchivedArticles();
+			let articles = await loaders.loadArchivedArticles();
+			articles = [...state.pendingArticles, ...articles];
 			for (let article of articles) {
-				refs.archivedArticlesList.innerHTML += /* html */ `
-					<li class="list-item">
+				await new Promise(r => setTimeout(r, 250));
+				const el = document.createElement('div');
+				el.innerHTML = /* html */ `
+					<li class="list-item animate slideInUp">
 						<a href="https://arweave.net/${article.id}" target="_blank">
-							<div class="list-item__image" style="background-image: url(${article.heroImageUrl});"></div>
+							${
+								article.heroImageUrl
+									? `
+									<div
+										class="list-item__image"
+										style="background-image: url(${article.heroImageUrl});">
+									</div>
+									`
+									: ''
+							}
 							<div class="list-item__content">
+								${article.isPending ? '<p style="color: red;"><b>Pending!</b></p>' : ''}
 								<h3>${article.title}</h3>
 								<p>📜${article.sample}</p>
 								<p>👩‍💻by ${article.author}</p>
-								<p style="text-transform: uppercase">${article.humanReadableSentiment} Sentiment</p>
+								<p style="text-transform: uppercase">${
+									article.humanReadableSentiment
+								} Sentiment</p>
 							</div>
 						</a>
 					</li>
 				`;
+				refs.archivedArticlesList.append(...el.childNodes);
 			}
 		}
 	};
@@ -118,6 +170,9 @@
 			}
 		`();
 		refs.donationAddressEl.innerHTML = walletDonationAddress;
+		state.pendingArticles.forEach(a =>
+			loaders.pollForSuccessfulTransaction(a.id)
+		);
 	})();
 
 	refs.closeButton.addEventListener('click', () => {
@@ -159,7 +214,6 @@
 					url: e.target.value
 				});
 				state.parseResult = scrapeRes.data.scrapeWebsite;
-				state.previewHTML = previewRes.data.archivePreview;
 				refs.previewArea.setAttribute(
 					'src',
 					`/preview?url=${encodeURIComponent(state.parseResult.originUrl)}`
@@ -174,8 +228,15 @@
 	});
 
 	refs.archiveButton.addEventListener('click', async e => {
-		alert('archiving ' + state.parseResult.title);
+		alert(
+			'Archiving ' +
+				state.parseResult.title +
+				'! Check back in a few minutes while Arweave secures your document to the blockweave.'
+		);
+		const parseResult = state.parseResult;
 		methods.startLoading();
+		methods.setArticlePreviewLoaded(false);
+		methods.setUrlInputValue('');
 		const publishRes = await graphql`
 			mutation archive($parseResult: ParseResultInput!) {
 				publishArchive(parseResult: $parseResult)
@@ -185,23 +246,21 @@
 		});
 		console.log(publishRes);
 		state.publishedArchiveId = publishRes.data.publishArchive;
-		let count = 0;
-		while (true) {
-			if (count++ > 1000) break;
-			try {
-				await new Promise(r => setTimeout(r, 1000));
-				const archivePublishStatus = await loaders.loadArchivePublishStatus();
-				console.log(archivePublishStatus);
-				if (archivePublishStatus == 'SUCCESS') {
-					alert(
-						'Successfully archived ' + 'arweave.net/' + state.publishedArchiveId
-					);
-					break;
-				}
-			} catch (e) {
-				console.error(e);
-			}
-		}
+		state.pendingArticles = [
+			{
+				id: state.publishedArchiveId,
+				title: parseResult.title,
+				sample: parseResult.sample,
+				author: parseResult.author,
+				humanReadableSentiment: '...',
+				wordCount: parseResult.wordCount,
+				heroImageUrl: parseResult.heroImageUrl,
+				isPending: true
+			},
+			...state.pendingArticles
+		];
+		await connectors.updateArchivedArticlesList();
+		await loaders.pollForSuccessfulTransaction(state.publishedArchiveId);
 		methods.stopLoading();
 		methods.setUrlInputValue('');
 	});
